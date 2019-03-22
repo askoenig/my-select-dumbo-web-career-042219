2ac47a207125416ca7115d3edc57c9c0846009b4
Andrew Koenig-Bautista <askoenig94@gmail.com>
2019-03-22 02:23:18 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,8 @@ def my_select(collection)
   i = 0
   new_array = []
   while i < collection.length
-  new_array<< (yield collection[i.include?()])
+  if
+  new_array << yield (collection[i])
     i = i + 1
   end
   new_array
